fb6ac680e2f63960971c79c2db56878f5e3edb22
Quezia Vieira <vieiraquezia24@gmail.com>
2024-11-21 14:05:45 -0300

    alt quezia

--- pokemon/pokemon.js
+++ pokemon/pokemon.js
@@ -1,32 +1,32 @@
 async function fetchPokemon() {
     const pokemonName = document.getElementById("pokemonName").value.trim();
-    if(!pokemonName) {
-        document.getElementById("errorMessage").textContent = "Por favor, digite o nome ou ID de um Pokémon"
+    if (!pokemonName) {
+        document.getElementById("errorMessage").textContent = "Por favor, digite o nome ou ID de um Pokémon";
         document.getElementById("errorMessage").style.display = "block";
-        return;
+        return
     }
-    document.getElementById("errorMessage").style.display = "none";
-    document.getElementById("loadingMessage").style.display = 'block';
+    document.getElementById("errorMessage").style.display = "none"
+    document.getElementById("loadingMessage").style.display = "block"
 
-    try {
-        const response = await fetch(`http://localhost:3000/pokemon/${pokemonName.toLowerCase()}`);
-        if (!response.ok) throw new Error("Pokémon não encontrado.");
 
-        const data = await response.json();
+try{
+    const response = await fetch(`http://localhost:3000/pokemon/${pokemonName.toLowerCase()}`);
+    if (!response.ok) throw new Error ("Pokémon não encontrado.");
 
-        document.getElementById("pokemonInfo").style.display = "block";
-        document.getElementById("pokemonTitle").textContent = data.name;
-        document.getElementById("pokemonImage").src = data.image;
-        document.getElementById("pokemonImage").alt = `Imagem de ${data.name}`;
-        document.getElementById("pokemonHeight").textContent = data.height;
-        document.getElementById("pokemonWeight").textContent = data.weight;
-        document.getElementById("pokemonAbilities").textContent = data.abilities;
-        document.getElementById("pokemonTypes").textContent = data.types;
-    } catch(error) {
-        document.getElementById("errorMessage").textContent = error.message;
-        document.getElementById("errorMessage").style.display = "block";
-        document.getElementById("pokemonInfo").style.display = "none";
-    } finally {
-        document.getElementById("loadingMessage").style.display = "none";
-    }
-}
+    const data = await response.json();
+
+    document.getElementById("pokemonInfo").style.display = "block";
+    document.getElementById("pokemonTitle").textContent = data.name;
+    document.getElementById("pokemonImage").src = data.image;
+    document.getElementById("pokemonImage").alt = `Imagem de ${data.name}`;
+    document.getElementById("pokemonHeight").textContent = data.height;
+    document.getElementById("pokemonWeight").textContent = data.weight;
+    document.getElementById("pokemonAbilities").textContent = data.abilities;
+    document.getElementById("pokemonTypes").textContent = data.types;
+}catch (error) {
+    document.getElementById("errorMessage").textContent = error.message;
+    document.getElementById("errorMessage").style.display = "block";
+    document.getElementById("pokemonInfo").style.display = "none";
+} finally {
+    document.getElementById("loadingMessage").style.display = "none";
+} }
